7fafd69df3641a2e9f79275ce8c756bf24a047ab
gustavo.carvalho <gustavo.stn@outlook.com.br>
2025-01-12 02:46:52 -0300

    fix: subsets for lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
 import { Lusitana, Montserrat } from "next/font/google";
 export const montserrat = Montserrat({ subsets: ["latin"] });
 // Secondary Font
-export const lusitana = Lusitana({ style: ["normal"], weight: ["400", "700"] });
+export const lusitana = Lusitana({
+  style: ["normal"],
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
