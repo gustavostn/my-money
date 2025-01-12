7d90e42235ee0c988e95438cf80fd016ee815323
gustavo.carvalho <gustavo.stn@outlook.com.br>
2025-01-11 22:36:33 -0300

    feat: learning about fonts and image optimization

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Montserrat } from "next/font/google";
+export const montserrat = Montserrat({ subsets: ["latin"] });
+// Secondary Font
+export const lusitana = Lusitana({ style: ["normal"], weight: ["400", "700"] });
