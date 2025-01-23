8de874444cfd869a5e307e80749570931e12b201
gustavo.carvalho <gustavo.stn@outlook.com.br>
2025-01-23 18:59:53 -0300

    feat: enable query's to get dashboard data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchInvoiceInfos,
+  fetchCustomers,
+  fetchCustomersQtty,
+} from "../lib/data";
 
-export default function Page() {
-  return <p>DashboardPage</p>;
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const { paidValue, pendingValue, totalQtty } = await fetchInvoiceInfos();
+  const numberOfCustomers = await fetchCustomersQtty();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={paidValue} type="collected" />
+        <Card title="Pending" value={pendingValue} type="pending" />
+        <Card title="Total Invoices" value={totalQtty} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
